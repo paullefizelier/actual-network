0e1e1784064a9689818867c7babb36484d58b180
Paul Lefizelier <paul@jobpass.com>
2026-06-22 14:52:05 +0200

    feat(auth): email/password login screen

---
+++ app/pages/confirm.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+definePageMeta({ layout: false })
+const user = useSupabaseUser()
+watchEffect(() => {
+  if (user.value) navigateTo('/')
+})
+</script>
+
+<template>
+  <div class="min-h-screen flex items-center justify-center">Connexion…</div>
+</template>
